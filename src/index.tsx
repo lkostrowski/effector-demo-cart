206ed49b714023c6bafe9684124748af91166a58
Lukasz Ostrowski <lukasz@monolog.tech>
2021-12-14 10:09:34 +0100

    Prep for cart

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,53 @@
 import ReactDOM from 'react-dom';
-import { PokemonList } from './pokemon-list';
+import { useStore } from 'effector-react';
+import { products$ } from './products';
+import {
+    Button,
+    ChakraProvider,
+    Text,
+    Container,
+    Grid,
+    Image,
+    Box,
+} from '@chakra-ui/react';
+
+const ProductsList = () => {
+    const products = useStore(products$);
+
+    return (
+        <Container className="grid">
+            {products.map((product) => (
+                <Box p={2} key={product.id}>
+                    <Image src={product.imageUrl} />
+                    <Text fontSize="lg">{product.name}</Text>
+                    <Text sx={{ my: 2 }} fontSize="sm">
+                        ${product.price}
+                    </Text>
+                    <Button isFullWidth colorScheme="blue">
+                        Add to cart
+                    </Button>
+                </Box>
+            ))}
+        </Container>
+    );
+};
+
+const Cart = () => {
+    return (
+        <Box border="1px solid #ddd" p={4}>
+            <Text fontSize="xl">Cart</Text>
+            <Text>TODO Implement me</Text>
+        </Box>
+    );
+};
 
 const App = () => (
-    <div>
-        <PokemonList />
-    </div>
+    <ChakraProvider>
+        <Grid templateColumns="3fr 1fr">
+            <ProductsList />
+            <Cart />
+        </Grid>
+    </ChakraProvider>
 );
 
 const app = document.getElementById('app');
---
+++ src/products.ts
@@ -0,0 +1,24 @@
+import { createStore } from 'effector';
+import { times, random } from 'lodash';
+
+type Product = {
+    id: string;
+    name: string;
+    price: number;
+    imageUrl: string;
+};
+
+const productType = ['jeans', 'coffee', 'flower', 'car'];
+
+const genProducts = (count: number): Product[] => {
+    return times(count).map(
+        (index): Product => ({
+            id: `${index}`,
+            imageUrl: `https://loremflickr.com/g/320/240/${productType[index]}`,
+            price: random(50, 150),
+            name: `Product ${productType[index]}`,
+        }),
+    );
+};
+
+export const products$ = createStore<Product[]>(genProducts(4));
